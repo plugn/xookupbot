888a8f8710fd6ff33322412400baf4c3594d8327
Maxim Dolgov <bananafishbone@gmail.com>
2017-04-20 19:49:24 +0300

    minor secure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = require('./conf.json').api_token; // 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = require('./.secret/conf.json').api_token; // 'YOUR_TELEGRAM_BOT_TOKEN';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
