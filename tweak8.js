3874bf83854c7280e27cca032f5a5af9e33a3304
Maxim Dolgov <bananafishbone@gmail.com>
2017-04-26 19:05:08 +0300

    cleanup

--- tweak8.js
+++
@@ -1,8 +0,0 @@
-const sharp = require('sharp');
-// const fs = require("fs");
-// const path = require('path');
-
-sharp('_img/HLV3.svg')
-	// .resize(300, 200)
-	.toFile('_img/HLV3.png', function(err) {
-	});
